f40f8cb03bf66f90057174adbec3ce98fb725690
Bharath ks <bharathks98408@gmail.com>
2026-04-23 17:28:07 +0530

    Update API URL to Render production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://sentiment-analysis-dashboard-xdbp.onrender.com/api';
+
 
 const getToken = () => localStorage.getItem('token');
 
